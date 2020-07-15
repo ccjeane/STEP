8543262c3d5c3175efbab3ed361541b2afa182ee
claudia <cjcoulibaly@gmail.com>
2020-07-15 15:37:06 +0000

    Passes all given tests.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -65,7 +65,18 @@ public final class FindMeetingQuery {
             for (int i = 0; i < guestSchedule.size(); i++){
                 t = guestSchedule.get(i);
                 if (st < t.start() && t.start() - st >= duration){
-                    available.add(TimeRange.fromStartEnd(st, t.start(), false));
+                    TimeRange possible = TimeRange.fromStartEnd(st, t.start(), false);
+                    boolean modified = false;
+                    // Accounts for double-booked schedules. 
+                    for (TimeRange x : guestSchedule){
+                        if (x.overlaps(possible)){
+                            modified = true;
+                            x = findIntersectionBetweenRanges(x, possible, duration);
+                        }
+                    }
+                    if (!modified){
+                        available.add(possible);
+                    }
                 } else {
                     st = t.end();
                 }
@@ -77,12 +88,72 @@ public final class FindMeetingQuery {
         }
         allTimes.add(available);
     }
-    //return allTimes.get(0);
+    
+    Collection<TimeRange> intersect = findIntersectionBetweenLists(allTimes, duration);
 
+    return intersect;
+  }
 
-    Collection<TimeRange> intersect = new ArrayList<>();
+  public List<TimeRange> findIntersectionBetweenLists(ArrayList<ArrayList<TimeRange>> allTimes, int duration){
+    List<TimeRange> intersection = new ArrayList<>();
+
+    // If there is only 1 guest, return when they are available.
     if (allTimes.size() == 1){
         return allTimes.get(0);
+    }
+
+    // Check each guest with the rest of the guestlist's schedule
+    for (int i = 0; i < allTimes.size() - 1; i++){
+        if (allTimes.get(i).size() > 0 && allTimes.get(i+1).size() > 0){
+            // Finds intersection between two TimeRanges. 
+            for (TimeRange t : allTimes.get(i)){
+                for (TimeRange c : allTimes.get(i + 1)){
+                    if (t.overlaps(c)){
+                        intersection.add(findIntersectionBetweenRanges(t, c, duration));
+                    }
+                }
+            }
+        } else {
+            // If any of the guests is busy for the entire day, just return an empty list
+            return new ArrayList<>();
+        }
+    } 
+    
+    return intersection;
+  }
+
+
+  // Finds the shared time between two TimeRanges
+  // If there is no intersection, returns null
+  public TimeRange findIntersectionBetweenRanges(TimeRange a, TimeRange b, int duration){
+      int start = Math.max(a.start(), b.start()); 
+      int end = Math.min(a.end(), b.end());
+      if (end - start >= duration) {
+          return TimeRange.fromStartEnd(start, end, false);
+      } 
+      return null;
+  }
+}
+    /*
+    if (allTimes.size() == 1){
+        if (allTimes.get(0).size() > 1){
+            for (int n = 0; n < allTimes.get(0).size() - 1; n++){
+                TimeRange current = allTimes.get(0).get(n);
+                for (int l = 1; l < allTimes.get(0).size(); l++){
+                    TimeRange next = allTimes.get(0).get(l);
+                    if (current.overlaps(next)){
+                        int overlapStart = Math.max(current.start(), next.start());
+                        int overlapEnd = Math.min(current.end(), next.end());
+                        if (overlapEnd - overlapStart >= duration){
+                            intersect.add(TimeRange.fromStartEnd(overlapStart, overlapEnd, false));
+                        }
+                    }
+                }
+            }
+            return intersect;
+        } else {
+            return allTimes.get(0);
+        }
     }
 
     for (int i = 0; i < allTimes.size() - 1; i++){
@@ -105,32 +176,21 @@ public final class FindMeetingQuery {
         }
     } 
 
-    return intersect;
-  }
-}
-
-/*
-    ArrayList<ArrayList<TimeRange>> allTimes = new ArrayList<>();
-    for (String guest : desiredGuests){
-        start = 0;
-        ArrayList<TimeRange> guestNotBusy = new ArrayList<>();
-        for (Event e : events){
-            // See if they are attending the event
-            if (e.getAttendees().contains(guest)){
-                // Get the time of the event so we can mark as busy until the end
-                TimeRange busy = e.getWhen();
-                
-                if (start < busy.start() && (busy.start() - start) >= duration){
-                    guestNotBusy.add(TimeRange.fromStartEnd(start, busy.start(), false));
-                } 
-                // Update their availability to begin after the current event.
-                start = busy.end();
+    intersection.addAll(allTimes.remove(0));
+     
+    while (allTimes.size() > 0){
+        List<TimeRange> current = allTimes.remove(0);
+        while (current.size() > 0){
+            TimeRange t = current.get(0);
+            boolean someOverlap = false;
+            for (TimeRange tr1 : intersection){
+                if (t.overlaps(tr1)){
+                    someOverlap = true;
+                    tr1 = findIntersectionBetweenRanges(t, tr1, duration);
+                }
             }
+            current.remove(0);
         }
-        if (latestTime - start >= duration){
-            guestNotBusy.add(TimeRange.fromStartEnd(start, latestTime, false));
-        }
-
-        allTimes.add(guestNotBusy);
     }
+
     */
